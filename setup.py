27664f91cd249ca2ac5b794cebff0f76e09b88cd
Keith Brady <34693973+krcb197@users.noreply.github.com>
2023-08-16 22:24:57 +0100

    Add support to 3.11 to Setup.py

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3 :: Only",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
